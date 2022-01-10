6e145156421891b581adccfd92239567219da427
coolpythoncodes <coolpythoncodes@gmail.com>
2022-01-10 14:46:45 +0100

    Letter avatar with status

--- src/stories/avatar/avatar.stories.js
+++ src/stories/avatar/avatar.stories.js
@@ -1,268 +1,588 @@
 import Avatar from "./avatar";
 
 export default {
-    component: Avatar,
-    title: "Avatar",
-}
+	component: Avatar,
+	title: "Avatar",
+};
 
 const Template = (args) => <Avatar {...args} />;
 
+// Letter avatars without status or image
 export const LetterAvatarSmallCyanLight = Template.bind({});
 LetterAvatarSmallCyanLight.args = {
-    name: 'Copper Unix',
-    colorScheme: 'cyan-light',
-    size: 'small',
+	name: "Copper Unix",
+	colorScheme: "cyan-light",
+	size: "small",
 };
 
 export const LetterAvatarMediumCyanLight = Template.bind({});
 LetterAvatarMediumCyanLight.args = {
-    name: 'Copper Unix',
-    colorScheme: 'cyan-light',
-    size: 'medium',
+	name: "Copper Unix",
+	colorScheme: "cyan-light",
+	size: "medium",
 };
 
 export const LetterAvatarLargeCyanLight = Template.bind({});
 LetterAvatarLargeCyanLight.args = {
-    name: 'Copper Unix',
-    colorScheme: 'cyan-light',
-    size: 'large',
+	name: "Copper Unix",
+	colorScheme: "cyan-light",
+	size: "large",
 };
 
 export const LetterAvatarSmallCyanBold = Template.bind({});
 LetterAvatarSmallCyanBold.args = {
-    name: 'Copper Unix',
-    colorScheme: 'cyan-bold',
-    size: 'small',
+	name: "Copper Unix",
+	colorScheme: "cyan-bold",
+	size: "small",
 };
 
 export const LetterAvatarMediumCyanBold = Template.bind({});
 LetterAvatarMediumCyanBold.args = {
-    name: 'Copper Unix',
-    colorScheme: 'cyan-bold',
-    size: 'medium',
+	name: "Copper Unix",
+	colorScheme: "cyan-bold",
+	size: "medium",
 };
 
 export const LetterAvatarLargeCyanBold = Template.bind({});
 LetterAvatarLargeCyanBold.args = {
-    name: 'Copper Unix',
-    colorScheme: 'cyan-bold',
-    size: 'large',
+	name: "Copper Unix",
+	colorScheme: "cyan-bold",
+	size: "large",
 };
 
 export const LetterAvatarSmallGreenLight = Template.bind({});
 LetterAvatarSmallGreenLight.args = {
-    name: 'Stephen Thomas',
-    colorScheme: 'green-light',
-    size: 'small',
+	name: "Stephen Thomas",
+	colorScheme: "green-light",
+	size: "small",
 };
 
 export const LetterAvatarMediumGreenLight = Template.bind({});
 LetterAvatarMediumGreenLight.args = {
-    name: 'Stephen Thomas',
-    colorScheme: 'green-light',
-    size: 'medium',
+	name: "Stephen Thomas",
+	colorScheme: "green-light",
+	size: "medium",
 };
 
 export const LetterAvatarLargeGreenLight = Template.bind({});
 LetterAvatarLargeGreenLight.args = {
-    name: 'Stephen Thomas',
-    colorScheme: 'green-light',
-    size: 'large',
+	name: "Stephen Thomas",
+	colorScheme: "green-light",
+	size: "large",
 };
 
 export const LetterAvatarSmallGreenBold = Template.bind({});
 LetterAvatarSmallGreenBold.args = {
-    name: 'Stephen Thomas',
-    colorScheme: 'green-bold',
-    size: 'small',
+	name: "Stephen Thomas",
+	colorScheme: "green-bold",
+	size: "small",
 };
 
 export const LetterAvatarMediumGreenBold = Template.bind({});
 LetterAvatarMediumGreenBold.args = {
-    name: 'Stephen Thomas',
-    colorScheme: 'green-bold',
-    size: 'medium',
+	name: "Stephen Thomas",
+	colorScheme: "green-bold",
+	size: "medium",
 };
 
 export const LetterAvatarLargeGreenBold = Template.bind({});
 LetterAvatarLargeGreenBold.args = {
-    name: 'Stephen Thomas',
-    colorScheme: 'green-bold',
-    size: 'large',
+	name: "Stephen Thomas",
+	colorScheme: "green-bold",
+	size: "large",
 };
 
- 
 export const LetterAvatarSmallOrangeLight = Template.bind({});
 LetterAvatarSmallOrangeLight.args = {
-    name: 'John Doe',
-    colorScheme: 'orange-light',
-    size: 'small',
+	name: "John Doe",
+	colorScheme: "orange-light",
+	size: "small",
 };
 
 export const LetterAvatarMediumOrangeLight = Template.bind({});
 LetterAvatarMediumOrangeLight.args = {
-    name: 'John Doe',
-    colorScheme: 'orange-light',
-    size: 'medium',
+	name: "John Doe",
+	colorScheme: "orange-light",
+	size: "medium",
 };
 
 export const LetterAvatarLargeOrangeLight = Template.bind({});
 LetterAvatarLargeOrangeLight.args = {
-    name: 'John Doe',
-    colorScheme: 'orange-light',
-    size: 'large',
+	name: "John Doe",
+	colorScheme: "orange-light",
+	size: "large",
 };
 
-  
 export const LetterAvatarSmallOrangeBold = Template.bind({});
 LetterAvatarSmallOrangeBold.args = {
-    name: 'John Doe',
-    colorScheme: 'orange-bold',
-    size: 'small',
+	name: "John Doe",
+	colorScheme: "orange-bold",
+	size: "small",
 };
 
 export const LetterAvatarMediumOrangeBold = Template.bind({});
 LetterAvatarMediumOrangeBold.args = {
-    name: 'John Doe',
-    colorScheme: 'orange-bold',
-    size: 'medium',
+	name: "John Doe",
+	colorScheme: "orange-bold",
+	size: "medium",
 };
 
 export const LetterAvatarLargeOrangeBold = Template.bind({});
 LetterAvatarLargeOrangeBold.args = {
-    name: 'John Doe',
-    colorScheme: 'orange-bold',
-    size: 'large',
+	name: "John Doe",
+	colorScheme: "orange-bold",
+	size: "large",
 };
 
-   
 export const LetterAvatarSmallRedLight = Template.bind({});
 LetterAvatarSmallRedLight.args = {
-    name: 'Christopher King',
-    colorScheme: 'red-light',
-    size: 'small',
+	name: "Christopher King",
+	colorScheme: "red-light",
+	size: "small",
 };
 
 export const LetterAvatarMediumRedLight = Template.bind({});
 LetterAvatarMediumRedLight.args = {
-    name: 'Christopher King',
-    colorScheme: 'red-light',
-    size: 'medium',
+	name: "Christopher King",
+	colorScheme: "red-light",
+	size: "medium",
 };
 
 export const LetterAvatarLargeRedLight = Template.bind({});
 LetterAvatarLargeRedLight.args = {
-    name: 'Christopher King',
-    colorScheme: 'red-light',
-    size: 'large',
+	name: "Christopher King",
+	colorScheme: "red-light",
+	size: "large",
 };
 
-    
 export const LetterAvatarSmallRedBold = Template.bind({});
 LetterAvatarSmallRedBold.args = {
-    name: 'Christopher King',
-    colorScheme: 'red-bold',
-    size: 'small',
+	name: "Christopher King",
+	colorScheme: "red-bold",
+	size: "small",
 };
 
 export const LetterAvatarMediumRedBold = Template.bind({});
 LetterAvatarMediumRedBold.args = {
-    name: 'Christopher King',
-    colorScheme: 'red-bold',
-    size: 'medium',
+	name: "Christopher King",
+	colorScheme: "red-bold",
+	size: "medium",
 };
 
 export const LetterAvatarLargeRedBold = Template.bind({});
 LetterAvatarLargeRedBold.args = {
-    name: 'Christopher King',
-    colorScheme: 'red-bold',
-    size: 'large',
+	name: "Christopher King",
+	colorScheme: "red-bold",
+	size: "large",
 };
 
- 
 export const LetterAvatarSmallGrayLight = Template.bind({});
 LetterAvatarSmallGrayLight.args = {
-    name: 'Younghoon Bingo Larry',
-    colorScheme: 'gray-light',
-    size: 'small',
+	name: "Younghoon Bingo Larry",
+	colorScheme: "gray-light",
+	size: "small",
 };
 
 export const LetterAvatarMediumGrayLight = Template.bind({});
 LetterAvatarMediumGrayLight.args = {
-    name: 'Younghoon Bingo Larry',
-    colorScheme: 'gray-light',
-    size: 'medium',
+	name: "Younghoon Bingo Larry",
+	colorScheme: "gray-light",
+	size: "medium",
 };
 
 export const LetterAvatarLargeGrayLight = Template.bind({});
 LetterAvatarLargeGrayLight.args = {
-    name: 'Younghoon Bingo Larry',
-    colorScheme: 'gray-light',
-    size: 'large',
+	name: "Younghoon Bingo Larry",
+	colorScheme: "gray-light",
+	size: "large",
 };
 
- 
 export const LetterAvatarSmallGrayBold = Template.bind({});
 LetterAvatarSmallGrayBold.args = {
-    name: 'Younghoon Bingo Larry',
-    colorScheme: 'gray-bold',
-    size: 'small',
+	name: "Younghoon Bingo Larry",
+	colorScheme: "gray-bold",
+	size: "small",
 };
 
 export const LetterAvatarMediumGrayBold = Template.bind({});
 LetterAvatarMediumGrayBold.args = {
-    name: 'Younghoon Bingo Larry',
-    colorScheme: 'gray-bold',
-    size: 'medium',
+	name: "Younghoon Bingo Larry",
+	colorScheme: "gray-bold",
+	size: "medium",
 };
 
 export const LetterAvatarLargeGrayBold = Template.bind({});
 LetterAvatarLargeGrayBold.args = {
-    name: 'Younghoon Bingo Larry',
-    colorScheme: 'gray-bold',
-    size: 'large',
+	name: "Younghoon Bingo Larry",
+	colorScheme: "gray-bold",
+	size: "large",
 };
-
 
 export const LetterAvatarSmallBlueLight = Template.bind({});
 LetterAvatarSmallBlueLight.args = {
-    name: 'Michael Jackson',
-    colorScheme: 'blue-light',
-    size: 'small',
+	name: "Michael Jackson",
+	colorScheme: "blue-light",
+	size: "small",
 };
 
 export const LetterAvatarMediumBlueLight = Template.bind({});
 LetterAvatarMediumBlueLight.args = {
-    name: 'Michael Jackson',
-    colorScheme: 'blue-light',
-    size: 'medium',
+	name: "Michael Jackson",
+	colorScheme: "blue-light",
+	size: "medium",
 };
 
 export const LetterAvatarLargeBlueLight = Template.bind({});
 LetterAvatarLargeBlueLight.args = {
-    name: 'Michael Jackson',
-    colorScheme: 'blue-light',
-    size: 'large',
+	name: "Michael Jackson",
+	colorScheme: "blue-light",
+	size: "large",
 };
 
-// 
+//
 export const LetterAvatarSmallBlueBold = Template.bind({});
 LetterAvatarSmallBlueBold.args = {
-    name: 'Michael Jackson',
-    colorScheme: 'blue-bold',
-    size: 'small',
+	name: "Michael Jackson",
+	colorScheme: "blue-bold",
+	size: "small",
 };
 
 export const LetterAvatarMediumBlueBold = Template.bind({});
 LetterAvatarMediumBlueBold.args = {
-    name: 'Michael Jackson',
-    colorScheme: 'blue-bold',
-    size: 'medium',
+	name: "Michael Jackson",
+	colorScheme: "blue-bold",
+	size: "medium",
 };
 
 export const LetterAvatarLargeBlueBold = Template.bind({});
 LetterAvatarLargeBlueBold.args = {
-    name: 'Michael Jackson',
-    colorScheme: 'blue-bold',
-    size: 'large',
+	name: "Michael Jackson",
+	colorScheme: "blue-bold",
+	size: "large",
+};
+
+// Letter avatars with status and no image
+export const LetterAvatarSmallCyanLightStatus = Template.bind({});
+LetterAvatarSmallCyanLightStatus.args = {
+	name: "Copper Unix",
+	colorScheme: "cyan-light",
+	size: "small",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarMediumCyanLightStatus = Template.bind({});
+LetterAvatarMediumCyanLightStatus.args = {
+	name: "Copper Unix",
+	colorScheme: "cyan-light",
+	size: "medium",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarLargeCyanLightStatus = Template.bind({});
+LetterAvatarLargeCyanLightStatus.args = {
+	name: "Copper Unix",
+	colorScheme: "cyan-light",
+	size: "large",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarSmallCyanBoldStatus = Template.bind({});
+LetterAvatarSmallCyanBoldStatus.args = {
+	name: "Copper Unix",
+	colorScheme: "cyan-bold",
+	size: "small",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarMediumCyanBoldStatus = Template.bind({});
+LetterAvatarMediumCyanBoldStatus.args = {
+	name: "Copper Unix",
+	colorScheme: "cyan-bold",
+	size: "medium",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarLargeCyanBoldStatus = Template.bind({});
+LetterAvatarLargeCyanBoldStatus.args = {
+	name: "Copper Unix",
+	colorScheme: "cyan-bold",
+	size: "large",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarSmallGreenLightStatus = Template.bind({});
+LetterAvatarSmallGreenLightStatus.args = {
+	name: "Stephen Thomas",
+	colorScheme: "green-light",
+	size: "small",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarMediumGreenLightStatus = Template.bind({});
+LetterAvatarMediumGreenLightStatus.args = {
+	name: "Stephen Thomas",
+	colorScheme: "green-light",
+	size: "medium",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarLargeGreenLightStatus = Template.bind({});
+LetterAvatarLargeGreenLightStatus.args = {
+	name: "Stephen Thomas",
+	colorScheme: "green-light",
+	size: "large",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarSmallGreenBoldStatus = Template.bind({});
+LetterAvatarSmallGreenBoldStatus.args = {
+	name: "Stephen Thomas",
+	colorScheme: "green-bold",
+	size: "small",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarMediumGreenBoldStatus = Template.bind({});
+LetterAvatarMediumGreenBoldStatus.args = {
+	name: "Stephen Thomas",
+	colorScheme: "green-bold",
+	size: "medium",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarLargeGreenBoldStatus = Template.bind({});
+LetterAvatarLargeGreenBoldStatus.args = {
+	name: "Stephen Thomas",
+	colorScheme: "green-bold",
+	size: "large",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarSmallOrangeLightStatus = Template.bind({});
+LetterAvatarSmallOrangeLightStatus.args = {
+	name: "John Doe",
+	colorScheme: "orange-light",
+	size: "small",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarMediumOrangeLightStatus = Template.bind({});
+LetterAvatarMediumOrangeLightStatus.args = {
+	name: "John Doe",
+	colorScheme: "orange-light",
+	size: "medium",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarLargeOrangeLightStatus = Template.bind({});
+LetterAvatarLargeOrangeLightStatus.args = {
+	name: "John Doe",
+	colorScheme: "orange-light",
+	size: "large",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarSmallOrangeBoldStatus = Template.bind({});
+LetterAvatarSmallOrangeBoldStatus.args = {
+	name: "John Doe",
+	colorScheme: "orange-bold",
+	size: "small",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarMediumOrangeBoldStatus = Template.bind({});
+LetterAvatarMediumOrangeBoldStatus.args = {
+	name: "John Doe",
+	colorScheme: "orange-bold",
+	size: "medium",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarLargeOrangeBoldStatus = Template.bind({});
+LetterAvatarLargeOrangeBoldStatus.args = {
+	name: "John Doe",
+	colorScheme: "orange-bold",
+	size: "large",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarSmallRedLightStatus = Template.bind({});
+LetterAvatarSmallRedLightStatus.args = {
+	name: "Christopher King",
+	colorScheme: "red-light",
+	size: "small",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarMediumRedLightStatus = Template.bind({});
+LetterAvatarMediumRedLightStatus.args = {
+	name: "Christopher King",
+	colorScheme: "red-light",
+	size: "medium",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarLargeRedLightStatus = Template.bind({});
+LetterAvatarLargeRedLightStatus.args = {
+	name: "Christopher King",
+	colorScheme: "red-light",
+	size: "large",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarSmallRedBoldStatus = Template.bind({});
+LetterAvatarSmallRedBoldStatus.args = {
+	name: "Christopher King",
+	colorScheme: "red-bold",
+	size: "small",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarMediumRedBoldStatus = Template.bind({});
+LetterAvatarMediumRedBoldStatus.args = {
+	name: "Christopher King",
+	colorScheme: "red-bold",
+	size: "medium",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarLargeRedBoldStatus = Template.bind({});
+LetterAvatarLargeRedBoldStatus.args = {
+	name: "Christopher King",
+	colorScheme: "red-bold",
+	size: "large",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarSmallGrayLightStatus = Template.bind({});
+LetterAvatarSmallGrayLightStatus.args = {
+	name: "Younghoon Bingo Larry",
+	colorScheme: "gray-light",
+	size: "small",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarMediumGrayLightStatus = Template.bind({});
+LetterAvatarMediumGrayLightStatus.args = {
+	name: "Younghoon Bingo Larry",
+	colorScheme: "gray-light",
+	size: "medium",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarLargeGrayLightStatus = Template.bind({});
+LetterAvatarLargeGrayLightStatus.args = {
+	name: "Younghoon Bingo Larry",
+	colorScheme: "gray-light",
+	size: "large",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarSmallGrayBoldStatus = Template.bind({});
+LetterAvatarSmallGrayBoldStatus.args = {
+	name: "Younghoon Bingo Larry",
+	colorScheme: "gray-bold",
+	size: "small",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarMediumGrayBoldStatus = Template.bind({});
+LetterAvatarMediumGrayBoldStatus.args = {
+	name: "Younghoon Bingo Larry",
+	colorScheme: "gray-bold",
+	size: "medium",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarLargeGrayBoldStatus = Template.bind({});
+LetterAvatarLargeGrayBoldStatus.args = {
+	name: "Younghoon Bingo Larry",
+	colorScheme: "gray-bold",
+	size: "large",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarSmallBlueLightStatus = Template.bind({});
+LetterAvatarSmallBlueLightStatus.args = {
+	name: "Michael Jackson",
+	colorScheme: "blue-light",
+	size: "small",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarMediumBlueLightStatus = Template.bind({});
+LetterAvatarMediumBlueLightStatus.args = {
+	name: "Michael Jackson",
+	colorScheme: "blue-light",
+	size: "medium",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarLargeBlueLightStatus = Template.bind({});
+LetterAvatarLargeBlueLightStatus.args = {
+	name: "Michael Jackson",
+	colorScheme: "blue-light",
+	size: "large",
+	status: "true",
+	statusColor: "green",
+};
+
+//
+export const LetterAvatarSmallBlueBoldStatus = Template.bind({});
+LetterAvatarSmallBlueBoldStatus.args = {
+	name: "Michael Jackson",
+	colorScheme: "blue-bold",
+	size: "small",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarMediumBlueBoldStatus = Template.bind({});
+LetterAvatarMediumBlueBoldStatus.args = {
+	name: "Michael Jackson",
+	colorScheme: "blue-bold",
+	size: "medium",
+	status: "true",
+	statusColor: "green",
+};
+
+export const LetterAvatarLargeBlueBoldStatus = Template.bind({});
+LetterAvatarLargeBlueBoldStatus.args = {
+	name: "Michael Jackson",
+	colorScheme: "blue-bold",
+	size: "large",
+	status: "true",
+	statusColor: "green",
 };
