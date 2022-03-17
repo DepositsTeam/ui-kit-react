d54c7f75fe6a57d451bb7d69580da4e6a3da2fdb
Kainy <iartkhay@gmail.com>
2022-03-17 01:05:34 +0100

    Removed dummy text from table

--- src/components/table/src/Table.js
+++ src/components/table/src/Table.js
@@ -7,7 +7,7 @@ import classNames from "../../../utils/classNames";
 import TableContent from "./TableContent;";
 import TableHeader from "./TableHeader";
 
-const Table = ({ data, headings , columns, className, ...props }) => {
+const Table = ({ data, headings, columns, className, ...props }) => {
 
     // const cols = Object.entries(data[0]).length
     const switchClassName = classNames(
@@ -16,20 +16,12 @@ const Table = ({ data, headings , columns, className, ...props }) => {
             // "ui-table__header": true,
             // "ui-table__content": true,
 
-         },
+        },
         className
     );
 
     return (
         <Box is={"div"} className={switchClassName} {...props} >
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, dolorum eaque maxime pariatur quibusdam iure quas a facere! Ipsam explicabo perferendis sed voluptatibus accusamus aperiam, nostrum beatae eaque odit vero.
-            <br />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam corrupti repudiandae enim natus vero accusantium, vitae exercitationem eligendi suscipit blanditiis necessitatibus eaque nisi officia consectetur quae autem nesciunt corporis quasi.
-            <br /> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, dolorum eaque maxime pariatur quibusdam iure quas a facere! Ipsam explicabo perferendis sed voluptatibus accusamus aperiam, nostrum beatae eaque odit vero.
-            <br />
-            <br />
-            <br />
-
             <TableHeader headings={headings} />
             <TableContent data={data} />
         </Box>
